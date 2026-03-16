0f09d4b4953a1c6d1145882293472727edcc142a
Jitendra Yadav <jitendraryadav45@gmail.com>
2026-03-16 15:09:20 +0530

    [UPDATE]:Hall model updated, search filters are added

--- lib/models/Hall.ts
+++ lib/models/Hall.ts
@@ -1,3 +1,38 @@
+// import mongoose from "mongoose";
+
+// const hallSchema = new mongoose.Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true,
+//     },
+
+//     description: String,
+
+//     location: {
+//       type: String,
+//       required: true,
+//     },
+
+//     pricePerHour: {
+//       type: Number,
+//       required: true,
+//     },
+
+//     images: [String],
+
+//     owner: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.models.Hall ||
+//   mongoose.model("Hall", hallSchema);
+
+
 import mongoose from "mongoose";
 
 const hallSchema = new mongoose.Schema(
@@ -9,15 +44,30 @@ const hallSchema = new mongoose.Schema(
 
     description: String,
 
-    location: {
+    city: {
       type: String,
+      required: true,
+      index: true
+    },
+
+    address: String,
+
+    capacity: {
+      type: Number,
       required: true,
     },
 
     pricePerHour: {
       type: Number,
       required: true,
+      index: true
     },
+
+    amenities: [
+      {
+        type: String
+      }
+    ],
 
     images: [String],
 
@@ -25,6 +75,12 @@ const hallSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+
+    isActive: {
+      type: Boolean,
+      default: true
+    }
+
   },
   { timestamps: true }
 );
